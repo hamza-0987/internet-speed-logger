e081742ab6275ad861283c58c0088413488b24c7
HAMZA <59221443+hamza-0987@users.noreply.github.com>
2025-07-01 01:44:40 +0500

    Update supabase_config.py

--- supabase_config.py
+++ supabase_config.py
@@ -1,10 +1,6 @@
 import os
-from supabase import create_client, Client
-from dotenv import load_dotenv
+from supabase import create_client
 
-load_dotenv()
-
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_KEY")
+supabase = create_client(url, key)
