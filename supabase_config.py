5f17a21600091ed4b6e44123458272134f276c61
HAMZA <59221443+hamza-0987@users.noreply.github.com>
2025-07-01 01:53:36 +0500

    Update supabase_config.py

--- supabase_config.py
+++ supabase_config.py
@@ -3,4 +3,8 @@ from supabase import create_client
 
 url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_KEY")
+
+if not url or not key:
+    raise ValueError("SUPABASE_URL and SUPABASE_KEY must be set as environment variables.")
+
 supabase = create_client(url, key)
